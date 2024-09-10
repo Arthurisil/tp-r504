e4ebfeeba84d3a755ad474aeaddea47363b703b2
cimpoart <cimpoiesproarthur@gmail.com>
2024-09-10 10:59:50 +0200

    Création fichier carré boucle

--- tp1/prog1.py
+++ tp1/prog1.py
@@ -1,5 +1,9 @@
-print ("Hello, World!")
 
-nombre = float(input("Entrez un nombre : "))
-carre = nombre ** 2
-print(f"Le carré de {nombre} est {carre}")
+while True:
+	try:
+		nombre = float(input("Entrez un nombre : "))
+		carre = nombre ** 2
+		print(f"Le carré de {nombre} est {carre}")
+	except KeyboardInterrupt:
+		print("\nÀ bientôt !")
+		break
