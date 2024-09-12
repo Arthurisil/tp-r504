0a0805b363036b8031c152212cbebffff744818b
cimpoart <cimpoiesproarthur@gmail.com>
2024-09-12 11:09:06 +0200

    testtest4

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -2,11 +2,11 @@ def puiss(a, b):
 	if not type(a) is int or not type(b) is int:
 		raise TypeError("Only integers are allowed")
 	if b < 0:
-		if a==0:
+		if a == 0:
 			raise ZeroDivisionError("0 ne peut pas être élevé à une puissance négative.")
 		raise ValueError("L'exposant doit être un entier positif ou zéro.")
 
-	resultat=1
+	resultat = 1
 	for _ in range(b):
 		resultat *= a
 		
