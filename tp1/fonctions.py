de9eb6f05f82f995056656aadd60bd8349aa1e72
cimpoart <cimpoiesproarthur@gmail.com>
2024-09-12 08:52:07 +0200

    3.2

--- tp1/fonctions.py
+++ tp1/fonctions.py
@@ -1,4 +1,4 @@
-def puissance(a, b):
-	if not isinstance(a, int) or not isinstance(b, int):
+def puiss(a, b):
+	if not type(a) is int or not type(b) is int:
 		raise TypeError("Only integers are allowed")
 	return a ** b
