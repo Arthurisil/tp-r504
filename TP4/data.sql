2c36e53c39c3f21e6112fa5262d0509143a63190
Arthurisil <cimpoiesproarthur@gmail.com>
2024-09-23 09:50:07 +0200

    blocage

--- TP4/data.sql
+++ TP4/data.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE demosql;
 USE demosql;
-CREATE TABLE myTable (id int AUTO_INCREMENT, name varchar(45) NOT NULL, PRIMARY KEY(id));
+CREATE TABLE myTable (id int AUTO_INCREMENT, name varchar(45) NOT NULL
+	, PRIMARY KEY(id));
 INSERT INTO myTable (id, name) VALUES (NULL, 'arthur');
 INSERT INTO myTable (id, name) VALUES (NULL, 'clement');
 INSERT INTO myTable (id, name) VALUES (NULL, 'matheo');
