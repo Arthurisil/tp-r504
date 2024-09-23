1504baabeaf2b8e41c0bab15d3258312ec6a0c70
Arthurisil <cimpoiesproarthur@gmail.com>
2024-09-23 14:27:16 +0200

    TP FLask

--- TP4/run_app.sh
+++ TP4/run_app.sh
@@ -5,13 +5,13 @@ IMAGE_NAME="im-tp4"
 NETWORK_NAME="net-tp4"
 PORT="5000"
 
-# Vérifier si existe déjà
-if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
-    echo "$CONTAINER_NAME existe déjà, il va être supprimé"
-    docker rm -f $CONTAINER_NAME
-fi
 
-# Lancer le new conteneur
+#if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
+ #   echo "$CONTAINER_NAME existe déjà, il va être supprimé"
+ #   docker rm -f $CONTAINER_NAME
+#fi
+
+
 echo "Lancement du conteneur $CONTAINER_NAME"
 docker run -d \
     --name $CONTAINER_NAME \
@@ -19,6 +19,5 @@ docker run -d \
     -p $PORT:$PORT \
     $IMAGE_NAME
 
-# Affiche le statut 
-docker ps -f name=$CONTAINER_NAME
+#docker ps -f name=$CONTAINER_NAME
 
