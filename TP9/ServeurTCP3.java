a72ebf8d20b1f85d6393d669c5336b6922a61dfd
Arthurisil <cimpoiesproarthur@gmail.com>
2024-10-15 15:38:29 +0200

    b

--- TP9/ServeurTCP3.java
+++ TP9/ServeurTCP3.java
@@ -1,21 +1,36 @@
-import java.io.*;   
-import java.net.*;  
+import java.io.*;
+import java.net.*;
 
-public class ServeurTCP2 
+public class ServeurTCP3 
 {
     public static void main(String[] args) throws IOException 
-    {
+  {
         ServerSocket socketserver = new ServerSocket(2016);
         System.out.println("Serveur en attente");
+
         while (true) 
-        {
-            Socket socket = socketserver.accept();
-            System.out.println("Connection d'un client");
-            DataInputStream dIn = new DataInputStream(socket.getInputStream());
-            System.out.println("Message: " + dIn.readUTF());
-//            socket.close();
-//            socketserver.close();
+      {
+            try 
+            {
+                Socket socket = socketserver.accept();
+                System.out.println("Connexion d'un client");
+
+                DataInputStream dIn = new DataInputStream(socket.getInputStream());
+                DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+
+                String message = dIn.readUTF();
+                System.out.println("Message reçu: " + message);
+
+                String rev = new StringBuilder(message).reverse().toString();
+                dOut.writeUTF(rev);
+                dOut.flush();
+
+                socket.close();
+            } catch (SocketException e) 
+            {
+                System.out.println("SocketException: " + e.getMessage());
+            }
         }
-    } 
+    }
 }
 
